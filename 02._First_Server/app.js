cb541e837de282380bebcffd43728afd906aec03
anderslatif <anderslatif@users.noreply.github.com>
2026-02-05 11:45:23 +0100

    Removed ESLint from the semester plan and finished the query string route

--- 02._First_Server/app.js
+++ 02._First_Server/app.js
@@ -13,6 +13,28 @@ app.get('/', (req, res) => {
 });
 
 
+// task create a new route called snowstorms, it should respond with a warning
+app.get('/snowstorms', (req, res) => {
+    res.send({ data: "Snowstorm is coming." });
+});
+
+// Question: how can we send data in a GET request?
+
+// path variable
+app.get('/cars/:carModel/:year', (req, res) => {
+    console.log(req.params);
+    res.send({ data: `
+        Your ${req.params.carModel} is very nice.
+        Is it from the year ${req.params.year}?
+    ` });
+});
+
+// query string / query parameters
+// ?parametername=value&parametertwo=valuetwo
+app.get("/bag", (req, res) => {
+    res.send({ data: req.query });
+});
+
 
 
 app.listen(8080);
