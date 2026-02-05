767f98b12e99a31d966ef61170c979b3094bd418
anderslatif <anderslatif@users.noreply.github.com>
2026-02-05 09:59:19 +0100

    Finished functions.js

--- 01._Miscellaneous/functions.js
+++ 01._Miscellaneous/functions.js
@@ -28,10 +28,28 @@ function genericActionPerformer(name, action) {
 // and use the genericActionPerformer to 
 // console.log `Valdemar is eating`
 
+function eatingAction(name) {
+    return `${name} is eating`;
+}
 
+console.log(genericActionPerformer('Valdemar', eatingAction));
+console.log(genericActionPerformer('Gustav', eatingAction));
 
+// task, run, Sidi
+// declare an anonymous function called runningAction
+// make it return `Sidi is running`
+// console.log it
 
+const runningAction = (name) => {
+    return `${name} is running`;
+};
 
+console.log(genericActionPerformer('Sidi', runningAction));
 
+// task: extra challenge
+// Write a one-liner below that uses genericActionPerformer
+// which prints: Kristian is laughing
+
+console.log(genericActionPerformer('Kristian', (name) => `${name} is laughing`));
 
 
